62056fde1b2d138e547f114a3584e75da8d7ecb5
Parm-M <76836936+Parm-M@users.noreply.github.com>
2022-01-20 13:31:49 +0530

    Add files via upload

---
+++ CollegeDB/index.php
@@ -0,0 +1,12 @@
+<?php
+// To include DB file to Index File
+  include_once "./includes/DB-inc.php";
+
+//Header
+  define("TITLE", "Home | College DB");
+  include_once("./includes/Header.php");
+
+  include("add.php"); // tables included
+  include("./includes/table.php");
+
+?>
